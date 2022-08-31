9f54a9289f451cd48f90aa8d6a32b368b0b93f74
arrivaldo <erickbarcena00@hotmail.com>
2022-08-31 02:14:50 -0500

    Layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Layout from "./components/Layout";
+import "./components/Layout.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const encabezados = document.querySelectorAll(".contenedor .encabezado");
+  const enlaces = document.querySelectorAll("#enlaces a");
+
+  const observer = new IntersectionObserver(
+    (entradas, observador) => {
+      entradas.forEach((entrada) => {
+        if (entrada.isIntersecting) {
+          const id = "#" + entrada.target.id;
+          window.history.pushState({}, entrada.target.innetText, id);
+
+          enlaces.forEach((enlace) => {
+            enlace.classList.remove("activo");
+
+            const href = enlace.attributes.href.nodeValue;
+            if (href === id) {
+              enlace.classList.add("activo");
+            }
+          });
+        }
+      });
+    },
+    {
+      threshold: 1,
+      rootMargin: "0px 0px -50% 0px",
+    }
   );
-}
 
+  encabezados.forEach((encabezado) => {
+    observer.observe(encabezado);
+  });
+
+  return (
+    <>
+      <Layout />
+    </>
+  );
+};
 export default App;
